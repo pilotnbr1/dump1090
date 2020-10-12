095da66faed03b4d01eeadcca5b91ddc9d521230
Stephen Oliver <steve@infincia.com>
2020-10-12 19:00:52 -0400

    Add openhd package script

---
+++ package.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+export LC_ALL=C.UTF-8
+export LANG=C.UTF-8
+
+PACKAGE_ARCH=$1
+OS=$2
+DISTRO=$3
+BUILD_TYPE=$4
+
+if [ "${BUILD_TYPE}" == "docker" ]; then
+    cat << EOF > /etc/resolv.conf
+options rotate
+options timeout:1
+nameserver 8.8.8.8
+nameserver 8.8.4.4
+EOF
+fi
+
+PACKAGE_NAME=dump1090
+
+TMPDIR=/tmp/${PACKAGE_NAME}-installdir
+
+rm -rf ${TMPDIR}/*
+
+mkdir -p ${TMPDIR}/usr/local/bin || exit 1
+
+make || exit 1
+cp dump1090 ${TMPDIR}/usr/local/bin/ || exit 1
+
+VERSION=$(git describe)
+
+rm ${PACKAGE_NAME}_${VERSION//v}_${PACKAGE_ARCH}.deb > /dev/null 2>&1
+
+fpm -a ${PACKAGE_ARCH} -s dir -t deb -n ${PACKAGE_NAME} -v ${VERSION//v} -C ${TMPDIR} \
+  -d "librtlsdr-dev" \
+  -p ${PACKAGE_NAME}_VERSION_ARCH.deb || exit 1
+
+#
+# Only push to cloudsmith for tags. If you don't want something to be pushed to the repo, 
+# don't create a tag. You can build packages and test them locally without tagging.
+#
+git describe --exact-match HEAD > /dev/null 2>&1
+if [[ $? -eq 0 ]]; then
+    echo "Pushing package to OpenHD repository"
+    cloudsmith push deb openhd/openhd-2-0/${OS}/${DISTRO} ${PACKAGE_NAME}_${VERSION//v}_${PACKAGE_ARCH}.deb || exit 1
+else
+    echo "Pushing package to OpenHD testing repository"
+    cloudsmith push deb openhd/openhd-2-0-testing/${OS}/${DISTRO} ${PACKAGE_NAME}_${VERSION//v}_${PACKAGE_ARCH}.deb || exit 1
+fi
+
